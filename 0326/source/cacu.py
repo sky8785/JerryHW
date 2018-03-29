5fa0c53beac4a847644c8416f7167e560314d22a
sky8785 <36830383+sky8785@users.noreply.github.com>
2018-03-29 14:52:02 +0800

    okay

---
+++ 0326/source/cacu.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+def ans(a,b,x):
+    if x == "+":
+        return a+b
+    elif x == "-":
+        return a-b
+    elif x == "*":
+        return a*b
+    elif x == "/":
+        return a/b
+
